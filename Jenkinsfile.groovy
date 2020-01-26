5526bbbe7b988c376bdf84cd3f4d27a6385be28c
egor_p <e.popelyaev@gmail.com>
2020-01-26 15:52:10 +0100

    fixed issue with the war in the docker file

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -33,7 +33,7 @@ pipeline {
             post {
                 success {
                     junit '**/target/surefire-reports/TEST-*.xml'
-                    archiveArtifacts 'target/*.jar'
+                    archiveArtifacts 'target/*.war'
                 }
             }
         }
