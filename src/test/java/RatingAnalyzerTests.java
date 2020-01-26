1c26cf1e1a07402627905966a6b3c2b025371b91
egor_p <e.popelyaev@gmail.com>
2020-01-26 23:27:40 +0100

    improved unit tests

--- src/test/java/RatingAnalyzerTests.java
+++ src/test/java/RatingAnalyzerTests.java
@@ -1,18 +1,25 @@
-import com.ebayk.data.user.UserRepository;
+import com.ebayk.data.user.User;
 import com.ebayk.service.RatingAnalyzer;
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
 
 public class RatingAnalyzerTests {
 
     @Test
-    public void getRatedUserForGivenRatingCreatorId() {
+    public void getRatedUserForGivenRatingCreatorId() throws RatingAnalyzer.UserNotFoundException {
+        List<User> ratedUsers = RatingAnalyzer.getRatedUserForRatingCreator(3);
+        Assert.assertEquals(1, (int) ratedUsers.get(0).getId());
     }
 
-    @Test
-    public void throwsUserNotFoundException_RatingCreatorNotExist() {}
+    @Test(expected = RatingAnalyzer.UserNotFoundException.class)
+    public void throwsUserNotFoundException_RatingCreatorNotExist() throws RatingAnalyzer.UserNotFoundException {
+        RatingAnalyzer.getRatedUserForRatingCreator(33);
+    }
 
-    @Test
-    public void throwUserNotFoundException_RatedUserNotExist() {}
-
+    @Test(expected = RatingAnalyzer.UserNotFoundException.class)
+    public void throwsUserNotFoundException_UserIdIsNull() throws RatingAnalyzer.UserNotFoundException {
+        RatingAnalyzer.getRatedUserForRatingCreator(null);
+    }
 }
